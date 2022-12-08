ed3d091491767c8302ac504ba98d5f7416ad3987
Edy Susilo <masedysu69@gmail.com>
2022-12-09 02:37:14 +0700

    update

--- index.php
+++ index.php
@@ -36,39 +36,8 @@ include('includes/config.php');
 
 <body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
-
-      <div class="logo">
-        <h1 class="fw-bold"><a href="index.html"><img src="assets/img/logoskh.png"><span>KORPRI SUKOHARJO</span></a>
-        </h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">BERANDA</a></li>
-          <li class="dropdown"><a href="#"><span>PROFIL </span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">SEJARAH KORPRI</a></li>
-              <li><a href="#">DASAR HUKUM</a></li>
-              <li><a href="#">VISI DAN MISI</a></li>
-              <li><a href="#">STRUKTUR ORGANISASI</a></li>
-              <li><a href="#">MAKNA LAMBANG KORPRI</a></li>
-              <li><a href="#">TUGAS POKOK DAN FUNGSI</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="kegiatan.html">KEGIATAN</a></li>
-          <li><a class="nav-link scrollto" href="#services">GALERI</a></li>
-          <li><a class="nav-link scrollto" href="#portfolio">PENGUMUMAN</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-    </div>
-  </header><!-- End Header -->
+  <!-- Header -->
+  <?php include('includes/header.php');?>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
