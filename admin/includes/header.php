31b6c0b66472119637d7dc883ad52d87df0bc44a
Edy Susilo <masedysu69@gmail.com>
2022-12-17 21:38:51 +0700

    update

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,11 +1,12 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header fixed-top d-flex align-items-center">
       <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+        <a href="index.php" class="logo d-flex align-items-center">
           <img src="assets/img/logoskh.png" alt="" />
           <span class="d-none d-lg-block">KORPRI SUKOHARJO</span>
         </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
+        
       </div>
       <!-- End Logo -->
       <nav class="header-nav ms-auto">
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,143 @@
+<aside id="sidebar" class="sidebar">
+      <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">
+            <i class="bi bi-grid"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <!-- End Dashboard Nav -->
+        <li class="nav-item">
+          <a
+            class="nav-link collapsed"
+            data-bs-target="#kegiatan-nav"
+            data-bs-toggle="collapse"
+            href="#"
+          >
+            <i class="bi bi-menu-button-wide"></i><span>Kegiatan Terbaru</span
+            ><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul
+            id="kegiatan-nav"
+            class="nav-content collapse"
+            data-bs-parent="#sidebar-nav"
+          >
+            <li>
+              <a href="add-post.php">
+                <i class="bi bi-plus-circle"></i><span>Add Post</span>
+              </a>
+            </li>
+            <li>
+              <a href="managepost.php">
+                <i class="bi bi-pencil"></i><span>Manage Post</span>
+              </a>
+            </li>
+            <li>
+              <a href="components-badges.html">
+                <i class="bi bi-trash"></i><span>Trash</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <!-- End Kegiatan Nav -->
+        <li class="nav-item">
+          <a
+            class="nav-link collapsed"
+            data-bs-target="#pengumuman-nav"
+            data-bs-toggle="collapse"
+            href="#"
+          >
+            <i class="bi bi-menu-button-wide"></i><span>Pengumuman</span
+            ><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul
+            id="pengumuman-nav"
+            class="nav-content collapse"
+            data-bs-parent="#sidebar-nav"
+          >
+            <li>
+              <a href="components-alerts.html">
+                <i class="bi bi-plus-circle"></i><span>Add Post</span>
+              </a>
+            </li>
+            <li>
+              <a href="components-accordion.html">
+                <i class="bi bi-pencil"></i><span>Manage Post</span>
+              </a>
+            </li>
+            <li>
+              <a href="components-badges.html">
+                <i class="bi bi-trash"></i><span>Trash</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <!-- End Pengumuman Nav -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="index.html">
+            <i class="bi bi-grid"></i>
+            <span>Struktur Organisasi</span>
+          </a>
+        </li>
+        <!-- End struktur Nav -->
+
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="users-profile.html">
+            <i class="bi bi-person"></i>
+            <span>Profile</span>
+          </a>
+        </li>
+        <!-- End Profile Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-faq.html">
+            <i class="bi bi-question-circle"></i>
+            <span>F.A.Q</span>
+          </a>
+        </li>
+        <!-- End F.A.Q Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-contact.html">
+            <i class="bi bi-envelope"></i>
+            <span>Contact</span>
+          </a>
+        </li>
+        <!-- End Contact Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-register.html">
+            <i class="bi bi-card-list"></i>
+            <span>Register</span>
+          </a>
+        </li>
+        <!-- End Register Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-login.html">
+            <i class="bi bi-box-arrow-in-right"></i>
+            <span>Login</span>
+          </a>
+        </li>
+        <!-- End Login Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-error-404.html">
+            <i class="bi bi-dash-circle"></i>
+            <span>Error 404</span>
+          </a>
+        </li>
+        <!-- End Error 404 Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="pages-blank.html">
+            <i class="bi bi-file-earmark"></i>
+            <span>Blank</span>
+          </a>
+        </li>
+        <!-- End Blank Page Nav -->
+      </ul>
+    </aside>
