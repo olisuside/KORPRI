a29cf02bc9fef017429652af66e23ddc60e782a4
Edy Susilo <masedysu69@gmail.com>
2022-12-20 23:25:27 +0700

    update

--- admin/manage-post.php
+++ admin/manage-post.php
@@ -123,9 +123,11 @@ include('includes/config.php');
                     <div class="card justify-content-center">
                         <div class="card-body">
                         <div class="d-sm-flex justify-content-between align-items-center">
-                            <h5 class="card-title">Table with hoverable rows</h5>
-                            <a class="btn btn-primary btn-sm col-md-2 mx-2" href="add-post.php">Tambah Produk</a>
+                            <h5 class="card-title">Postingan</h5>
+                            <a class="btn btn-primary btn-sm col-md-2 mx-2" href="add-post.php">Tambah Postingan</a>
                         </div>
+                        <div class="table-responsive text-nowrap">
+
                             <!-- Table with hoverable rows -->
                             <table class="table table-hover">
                                 <thead>
@@ -146,7 +148,7 @@ include('includes/config.php');
                                     </tr>
                                     <tr>
                                        
-                                        <td>Brandon Jacob</td>
+                                        <td>agsdhafjklsdjfhkla;gkkjhsanmdsdhgnsjhdvdsfbucbfjsdiufuewhsad;;kkjjfh</td>
                                         <td>Designer</td>
                                         <td><a href="edit-post.php?pid=<?php echo htmlentities($row['postid']);?>"><i class="bi bi-pencil-fill" style="color: #29b6f6;"></i></a> 
     &nbsp;<a href="manage-posts.php?pid=<?php echo htmlentities($row['postid']);?>&&action=del" onclick="return confirm('Do you reaaly want to delete ?')"> <i class="bi bi-trash" style="color: #f05050"></i></a> </td>
@@ -162,7 +164,7 @@ include('includes/config.php');
                                     </tr>
                                 </tbody>
                             </table>
-
+                            </div>
                         </div>
                     </div>
                 </div><!-- End columns -->
