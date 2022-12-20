d771d52863fdc166e3f2e7adaa3e778cf09e9af8
Edy Susilo <masedysu69@gmail.com>
2022-12-20 23:14:31 +0700

    update manage

--- admin/manage-post.php
+++ admin/manage-post.php
@@ -105,12 +105,12 @@ include('includes/config.php');
     <main id="main" class="main">
 
         <div class="pagetitle">
-            <h1>Tambah Postingan</h1>
+            <h1>Kelola Postingan</h1>
             
             <nav>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item active">Tambah Postingan</li>
+                    <li class="breadcrumb-item active">Kelola Postingan</li>
                 </ol>
             </nav>
         </div><!-- End Page Title -->
@@ -124,54 +124,41 @@ include('includes/config.php');
                         <div class="card-body">
                         <div class="d-sm-flex justify-content-between align-items-center">
                             <h5 class="card-title">Table with hoverable rows</h5>
-                            <button class="btn btn-info col-md-2 mx-2">Tambah Produk</button>
+                            <a class="btn btn-primary btn-sm col-md-2 mx-2" href="add-post.php">Tambah Produk</a>
                         </div>
                             <!-- Table with hoverable rows -->
                             <table class="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Position</th>
-                                        <th scope="col">Age</th>
-                                        <th scope="col">Start Date</th>
+                                        <th scope="col">Judul Kegiatan</th>
+                                        <th scope="col">Gambar</th>
+                                        <th scope="col">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <th scope="row">1</th>
+                                       
                                         <td>Brandon Jacob</td>
                                         <td>Designer</td>
-                                        <td>28</td>
-                                        <td>2016-05-25</td>
+                                        <td><a href="edit-post.php?pid=<?php echo htmlentities($row['postid']);?>"><i class="bi bi-pencil-fill" style="color: #29b6f6;"></i></a> 
+    &nbsp;<a href="manage-posts.php?pid=<?php echo htmlentities($row['postid']);?>&&action=del" onclick="return confirm('Do you reaaly want to delete ?')"> <i class="bi bi-trash" style="color: #f05050"></i></a> </td>
+                                      
                                     </tr>
                                     <tr>
-                                        <th scope="row">2</th>
-                                        <td>Bridie Kessler</td>
-                                        <td>Developer</td>
-                                        <td>35</td>
-                                        <td>2014-12-05</td>
+                                       
+                                        <td>Brandon Jacob</td>
+                                        <td>Designer</td>
+                                        <td><a href="edit-post.php?pid=<?php echo htmlentities($row['postid']);?>"><i class="bi bi-pencil-fill" style="color: #29b6f6;"></i></a> 
+    &nbsp;<a href="manage-posts.php?pid=<?php echo htmlentities($row['postid']);?>&&action=del" onclick="return confirm('Do you reaaly want to delete ?')"> <i class="bi bi-trash" style="color: #f05050"></i></a> </td>
+                                        
                                     </tr>
                                     <tr>
-                                        <th scope="row">3</th>
-                                        <td>Ashleigh Langosh</td>
-                                        <td>Finance</td>
-                                        <td>45</td>
-                                        <td>2011-08-12</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Angus Grady</td>
-                                        <td>HR</td>
-                                        <td>34</td>
-                                        <td>2012-06-11</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Raheem Lehner</td>
-                                        <td>Dynamic Division Officer</td>
-                                        <td>47</td>
-                                        <td>2011-04-19</td>
+                                       
+                                        <td>Brandon Jacob</td>
+                                        <td>Designer</td>
+                                        <td><a href="edit-post.php?pid=<?php echo htmlentities($row['postid']);?>"><i class="bi bi-pencil-fill" style="color: #29b6f6;"></i></a> 
+    &nbsp;<a href="manage-posts.php?pid=<?php echo htmlentities($row['postid']);?>&&action=del" onclick="return confirm('Do you reaaly want to delete ?')"> <i class="bi bi-trash" style="color: #f05050"></i></a> </td>
+                                      
                                     </tr>
                                 </tbody>
                             </table>
