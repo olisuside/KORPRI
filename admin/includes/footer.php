4376f1ae2d4f61d080dc7aef01dfa99f45bdf2ab
Edy Susilo <masedysu69@gmail.com>
2023-01-03 19:26:29 +0700

    add and remove

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Korpri Sukoharjo</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by Korpri Sukoharjo
+      </div>
+    </footer>
+    <!-- End Footer -->
---
+++ admin/remove-announcement.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+} else {
+    $postid = $_GET['id'] ;
+    $query = mysqli_query($con, "UPDATE tblpost set Is_Active=0 where id='$postid'");
+    if($query)
+{
+echo "Post deleted ";
+}
+else{
+echo"Something went wrong . Please try again.";    
+} 
+    header('location:manage-announcement.php');
+    
+}
