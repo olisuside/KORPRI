c222c8be5b88c2417c9aa8d05b07cbc601d03141
Edy Susilo <masedysu69@gmail.com>
2023-01-08 21:08:21 +0700

    add

---
+++ visi-misi.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>KORPRI SUKOHARJO</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logoskh.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include('includes/header.php'); ?><!-- End Header -->
+
+  <main id="main">
+    <!-- berita terbaru -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+        <div class="row justify-content-center">
+
+          <!-- Blog Entries Column -->
+          <div class="col-md-9">
+
+            <h3><strong> VISI & MISI KORPRI</strong></h3>
+            <img class="imgcenter" style="object-fit:cover;" src="assets/img/logo.png" alt="logo">
+            <div class="card-body">
+              <p>
+              </p>
+              <p><strong>
+                Visi KORPRI</strong></p>
+              <p>
+                Visi KORPRI adalah terwujudnya KORPRI sebagai organisasi yang
+                kuat, netral, mandiri, profesional dan terdepan dalam menjaga persatuan
+                dan kesatuan bangsa, menyejahterakan anggota, masyarakat, dan
+                melindungi kepentingan para anggota agar lebih profesional di dalam
+                membangun Pemerintahan yang baik (AD, Pasal 6).</p>
+              <p><strong>Misi KORPRI</strong></p>
+              <p>Misi KORPRI sebagaimana tertuang dalam AD, Pasal 9 adalah :</p>
+              <p>1). Mewujudkan organisasi KOPRPRI sebagai alat pemersatu bangsa dan
+                Negara</p>
+              <p>2). Memperkuat kedudukan, wibawa, dan martabat organisasi KOPRPRI</p>
+              <p>3). Meningkatkan peran serta KORPRI dalam menyukseskan
+                pemabangunan nasional</p>
+              <p>4). Menigkatkan perlindungan hukum dan pengayoman kepada anggota</p>
+              <p>5). Meningkatkan ketaqwaan dan profesionalitas anggota</p>
+              <p>6). Meningkatkan kesejahteraan anggota dan keluarganya</p>
+              <p>7). Menegakkan peratuan perundang-undangan Pegawai Republik
+                Indonesia</p>
+              <p>8). Mewujudkan rasa kesetiakawanan dan solidaritas sesama anggota
+                KORPRI;</p>
+              <p>9). Mewujudkan prinsip-prinsip kepemerintahan yang baik
+              </p>
+            </div>
+          </div>
+          </div>
+          
+          </section>
+          <?php include('includes/footer.php'); ?><!-- End Footer -->
