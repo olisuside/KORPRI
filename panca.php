d20b94d86da73ca23a8e515c06c491eccc50936c
Edy Susilo <masedysu69@gmail.com>
2023-03-07 22:07:13 +0700

    dfghjkl

--- panca.php
+++ panca.php
@@ -31,6 +31,7 @@ include('includes/config.php');
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
